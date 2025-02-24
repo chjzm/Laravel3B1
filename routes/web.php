245c38ef81aab2b51df2a3e7ed683c2e50c2ed62
CHAJZ G. BOLETIN <2010550@cc.usls.edu>
2025-02-24 08:57:27 +0800

    Feat: Seeding data into database

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-Route::get(uri:'/', action: function (): View {
+Route::get('/', function () {
     return view('welcome');
 });
-Route::get( '/login', 'App\Http\Controller@index');
+Route:: get('/item', 'App\Http\Controllers\ItemController@index');
+Route:: get('/login', 'App\Http\Controllers\LoginController@index');
+
+Route:: get('/aboutme', 'App\Http\Controllers\AboutmeController@index');
+
+Route:: get('/hello', 'App\Http\Controllers\HelloController@index');
+Route:: get('/inventory', 'App\Http\Controllers\InventoryController@index');
